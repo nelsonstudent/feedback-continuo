19c334b4288b7b2681b06ecdae1eda72977bde5b
Nelson <79551671+nelsonstudent@users.noreply.github.com>
2025-06-10 15:34:40 -0300

    Update main.py
    Atualização do main para poder usar o banco com SQLAlchemy.

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
+import models
+from database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-class Feedback(BaseModel):
+# Pydantic schema
+class FeedbackSchema(BaseModel):
     nome: str | None = None
     aula: str
     nota: int
     comentario: str
 
-feedbacks = []
+    class Config:
+        orm_mode = True
+
+# Dependency para pegar a sessão do banco
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.post("/feedback")
-def receber_feedback(fb: Feedback):
-    feedbacks.append(fb)
-    return {"msg": "Feedback recebido com sucesso!"}
+def criar_feedback(fb: FeedbackSchema, db: Session = Depends(get_db)):
+    feedback = models.Feedback(**fb.dict())
+    db.add(feedback)
+    db.commit()
+    db.refresh(feedback)
+    return feedback
 
 @app.get("/feedbacks")
-def listar_feedbacks():
-    return feedbacks
+def listar_feedbacks(db: Session = Depends(get_db)):
+    return db.query(models.Feedback).all()
