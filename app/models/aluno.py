8a8685f2f2a8c6e52cbb7a034cf0c570d51b9c81
Thomas Lesselis <thomas.lesselis@gmail.com>
2025-06-29 16:00:16 -0300

    Implementa o hash na senha para evitar armazenamento explícito de dados sensíveis. Insere método que define a rota de acordo com o tipo: aluno ou professor.

--- app/models/aluno.py
+++ app/models/aluno.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String
 from app.database.sias_db import Base
 from app.models.base import BaseModel
 from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class Aluno(Base, BaseModel):
     __tablename__ = "alunos"
@@ -10,8 +11,17 @@ class Aluno(Base, BaseModel):
     nome = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     turma = Column(String, nullable=False)
+    senha_hash = Column(String, nullable=False)
 
     grupos = relationship("GrupoAula", secondary="aluno_grupo", back_populates="alunos")
     materiais_visualizados = relationship("MaterialVisualizado", back_populates="aluno")
     respostas_pre_teste = relationship("RespostaPreTeste", back_populates="aluno")
     avaliacoes = relationship("Avaliacao", back_populates="aluno")
+    feedbacks_recebidos = relationship("Feedback", back_populates="aluno")
+    feedbacks_enviados = relationship("Feedback", back_populates="professor")
+
+    def set_senha(self, senha):
+        self.senha_hash = generate_password_hash(senha)
+    
+    def verificar_senha(self, senha):
+        return check_password_hash(self.senha_hash, senha)
