8f981a2cc562fe2534b38763d1fd003060a312a9
Thomas Lesselis <thomas.lesselis@gmail.com>
2025-06-29 15:56:34 -0300

    Importa dotenv e implementa camada de segurança com o flask_jwt.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from app.routes import routes_bp
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 def create_app(testing=False):
     app = Flask(__name__)
+    app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
+    jwt = JWTManager(app)
     app.register_blueprint(routes_bp)
     if testing:
         app.config["TESTING"] = True
