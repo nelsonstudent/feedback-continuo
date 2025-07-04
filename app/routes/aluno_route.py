66e5149d46893c69ff0cd88a1be349f1ccf467eb
Thomas Lesselis <thomas.lesselis@gmail.com>
2025-07-03 22:35:15 -0300

    Insere o método to_dict no response de criar aluno e professor. Evita a exposição de dados sensíveis.

--- app/routes/aluno_route.py
+++ app/routes/aluno_route.py
@@ -20,6 +20,7 @@ def listar_alunos():
 @aluno_bp.route('/<int:aluno_id>', methods=['GET'], endpoint='buscar_aluno')
 @jwt_required()
 def buscar_aluno(aluno_id):
+    print("DEBUG: /alunos/ endpoint chamado")
     aluno = buscar_aluno_por_id_service(aluno_id)
     if aluno:
         return jsonify(aluno)
@@ -29,7 +30,7 @@ def buscar_aluno(aluno_id):
 def criar_aluno():
     data = request.json
     nova_aluno = criar_aluno_service(data)
-    return jsonify(nova_aluno), 201
+    return jsonify(nova_aluno.to_dict()), 201
 
 @aluno_bp.route('/<int:aluno_id>', methods=['PUT'], endpoint='atualizar_aluno')
 @jwt_required()
