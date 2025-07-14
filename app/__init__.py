a5d07ed5658e4f3b911b63520646750375b16cbb
Ceu <ceu.ferreira05@gmail.com>
2025-07-13 22:18:34 -0300

    Atualizacao Init

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,27 @@
-from flask import Flask
-from app.routes import routes_bp
-from flask_jwt_extended import JWTManager
-from dotenv import load_dotenv
-load_dotenv()
-import os
+# app/__init__.py
 
-def create_app(testing=False):
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.config import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
+
+# ======== AQUI DEFINIMOS O DB ===================
+db = SQLAlchemy()
+
+def create_app():
     app = Flask(__name__)
-    app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
-    jwt = JWTManager(app)
-    app.register_blueprint(routes_bp)
-    if testing:
-        app.config["TESTING"] = True
+
+    # Configurações
+    app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
+
+    # Inicializa extensão
+    db.init_app(app)
+
+    # Registra blueprints (APIs)
+    from app.api.report_api import bp as report_bp
+    app.register_blueprint(report_bp)
+
+    # Inicia o scheduler em background (sem criar outro app)
+    from app.utils.scheduler import scheduler  # noqa: F401
+
     return app
