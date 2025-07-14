0147b8e91929eb6646dc5303efb1d243a20fbbf4
Ceu <ceu.ferreira05@gmail.com>
2025-07-14 00:08:20 -0300

    Atualiza scheduler.py

---
+++ app/utils/scheduler.py
@@ -0,0 +1,20 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+from app.repositorys.calendar_repository import listar_eventos_hoje
+from app.services.report_service import gerar_relatorio
+from datetime import datetime, timedelta
+
+scheduler = BackgroundScheduler()
+def checar_e_gerar():
+    agora = datetime.now()
+    for evt in listar_eventos_hoje():
+        fim = datetime.fromisoformat(evt['end']['dateTime'])
+        if fim <= agora <= fim + timedelta(minutes=5):
+            pass
+        try:
+            gerar_relatorio(evt['id'])
+            print(f"Relatório gerado para o evento {evt['id']}")
+        except Exception as e:
+             print(f"[Erro ao gerar relatório para o evento] {evt['id']}: {e}")     
+    print("Verificação de eventos concluída.")
+scheduler.add_job(checar_e_gerar, 'interval', minutes=5)
+scheduler.start()
