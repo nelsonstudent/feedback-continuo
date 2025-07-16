9db2798afed57cdc7bbcd2f841d4b5b8043b5b75
Ceu <ceu.ferreira05@gmail.com>
2025-07-16 00:49:16 -0300

    chore: registrar blueprint da avaliação de professores (US0005)

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, app
 from app.routes import routes_bp
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 load_dotenv()
 import os
-
+from app.api.avaliacao_professor_api import bp as evaluation_bp
+app.register_blueprint(evaluation_bp)
 def create_app(testing=False):
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
