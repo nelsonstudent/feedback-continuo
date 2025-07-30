79cd34fd0c4e059ce5129e5b5b47e0d6eab65aa0
Thomas Lesselis <thomas.lesselis@gmail.com>
2025-07-30 13:48:00 -0300

    Atualiza arquivos de inicialização para lidar com requisições externas. Inserção do CORS.

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,24 @@
 from flask import Flask
-from app.routes import routes_bp
+from flask_cors import CORS
+from routes import routes_bp
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
-load_dotenv()
 import os
+
+load_dotenv()
 
 def create_app(testing=False):
     app = Flask(__name__)
+
+    # CORS global cobre todas as rotas e blueprints
+    CORS(
+        app,
+        origins=["http://localhost:3000", "http://sias-frontend:3000"],
+        supports_credentials=True,
+        allow_headers=["Content-Type", "Authorization"],
+        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+    )
+
     app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
     jwt = JWTManager(app)
     app.register_blueprint(routes_bp)
