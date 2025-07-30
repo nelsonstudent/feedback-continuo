b4d2570f2e75290f375d0dfc026e611a8cb15e8c
Thomas Lesselis <thomas.lesselis@gmail.com>
2025-07-30 14:07:38 -0300

    Atualiza os imports e diretórios para lidar com as requisições externas.

--- app/database/cria_tabelas.py
+++ app/database/cria_tabelas.py
@@ -1,15 +1,19 @@
-from app.database.sias_db import Base, engine
-import app.models.aluno
-import app.models.professor
-import app.models.material
-import app.models.aula
-import app.models.grupo_aula
-import app.models.pre_teste
-import app.models.avaliacao
-import app.models.relatorio
-import app.models.material_visualizado
-import app.models.resposta_pre_teste
-import app.models.aula_material
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from database import Base, engine
+import models.aluno
+import models.professor
+import models.material
+import models.aula
+import models.grupo_aula
+import models.pre_teste
+import models.avaliacao
+import models.relatorio
+import models.material_visualizado
+import models.resposta_pre_teste
+import models.aula_material
 
 
 Base.metadata.create_all(bind=engine)
--- app/database/sias_db.py
+++ app/database/sias_db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./sias.db"
+DATABASE_URL = "sqlite:///db/sias.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
